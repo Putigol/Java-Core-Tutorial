c854e3784dde38c7fd4e1f263e3289d03deca640
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-20 23:19:05 +0700

    Arithmetic operators

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,34 +1,19 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        // MAD LIBS GAME
+        // Arithmetic Operators
 
-        Scanner scanner = new Scanner(System.in);
+        int x = 10;
+        int y = 2;
+        int z;
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        z = x + y; // ADDITION
+        z = x - y; // SUBTRACTION
+        z = x * y; // MULTIPLICATION
+        z = x / y; // DIVISION
+        z = x % y; // MODULUS
 
-        System.out.print("Enter an adjective (description): ");
-        adjective1 = scanner.nextLine();
-        System.out.print("Enter a noun (animal or person): ");
-        noun1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description): ");
-        adjective2 = scanner.nextLine();
-        System.out.print("Enter a verb ending with -ing (action): ");
-        verb1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description): ");
-        adjective3 = scanner.nextLine();
+        System.out.println(z);
 
-        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
-        System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was " + adjective3 + "!");
-
-        scanner.close();
     }
 }
