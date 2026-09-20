f472c17c5528cce06d7f3a79ce03c928cc7d77e7
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-20 23:24:59 +0700

    Code a simple java shopping cart program

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,19 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        // Arithmetic Operators
+        // SHOPPING CART PROGRAM
 
-        int x = 10;
-        int y = 2;
-        int z;
+        Scanner scanner = new Scanner(System.in);
 
-        z = x + y; // ADDITION
-        z = x - y; // SUBTRACTION
-        z = x * y; // MULTIPLICATION
-        z = x / y; // DIVISION
-        z = x % y; // MODULUS
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
 
-        System.out.println(z);
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
 
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + item +"/s");
+        System.out.println("Your total is " + currency + total);
+
+        scanner.close();
     }
 }
