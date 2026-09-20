3cfc9a8b5a129d61de9ddddfc9d9b99622e072b5
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-20 22:46:40 +0700

    Start coding with Java

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // THIS IS MY FIRST Java PROGRAM
+
+        /*
+        This
+        is
+        a
+        multi-line
+        comment
+         */
+
+        System.out.print("I like pizza!\n");
+        System.out.print("It's really good!\n");
+        System.out.print("Buy me pizza!\n");
+    }
+}
