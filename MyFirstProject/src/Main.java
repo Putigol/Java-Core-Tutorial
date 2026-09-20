8e4e4573b371ee777e7c5d3d9abce3716a74d3f1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-20 23:10:22 +0700

    Mad libs game

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -3,30 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // MAD LIBS GAME
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
-        System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
 
-        System.out.println("What is your gpa: ");
-        double gpa = scanner.nextDouble();
-
-        System.out.println("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your gpa is: " + gpa);
-
-        if(isStudent){
-            System.out.println("You are enrolled in classes");
-        }
-        else{
-            System.out.println("You are NOT enrolled in classes");
-        }
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
 
         scanner.close();
     }
