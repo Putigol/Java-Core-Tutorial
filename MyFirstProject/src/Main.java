82e1636abf4c5d3bc174031f5568e89e1b548499
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-20 22:59:06 +0700

    Variables

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,19 +1,49 @@
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
+        // ❎ variable = A reusable container for a value.
+        //                          A variable behaves as if it was the value it contains.
 
-        // THIS IS MY FIRST Java PROGRAM
+        // 🟥 Primitive = simple value stored directly in memory (stack)
+        // 🟦 Reference = memory address (stack) that points to the (heap)
 
-        /*
-        This
-        is
-        a
-        multi-line
-        comment
-         */
+        // 🟥 Primitive vs 🟦 Reference
+        //    ---------                      ---------
+        //    int                           string
+        //    double                    array
+        //    char                        object
+        //    boolean
 
-        System.out.print("I like pizza!\n");
-        System.out.print("It's really good!\n");
-        System.out.print("Buy me pizza!\n");
+        int age = 21;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name = "Bro Code";
+        String food = "pizza";
+        String email = "fake123@gmail.com";
+        String car = "Mustang";
+        String color = "red";
+
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is: " + currency + price);
+
+        if(forSale){
+            System.out.println("There is a " + car + " for sale");
+        }
+        else{
+            System.out.println("The " + car + " is not for sale");
+        }
     }
 }
