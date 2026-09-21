29f9f2688af2508cba6d3108fc40b15db7f7d7c1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-21 15:50:41 +0700

    Code a Java compound interest calculator

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,61 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        // printf() is a method used to format output
+        // Compound interest calculator
 
-        // % [flags] [width] [.precision] [specifier-character]
+        Scanner scanner = new Scanner(System.in);
 
-        // [specifier-character]
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
 
-        String name = "Spongebob";
-        char firstLetter = 'S';
-        int age = 30;
-        double height = 60.5;
-        boolean isEmployed = true;
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
 
-        System.out.printf("Hello %s\n", name);
-        System.out.printf("Your name starts with a %c\n", firstLetter);
-        System.out.printf("You are %d years old\n", age);
-        System.out.printf("You are %f inches tall\n", height);
-        System.out.printf("Employed: %b\n", isEmployed);
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
 
-        System.out.printf("%s is %d years old", name, age);
+        System.out.print("Enter the # of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
 
-        // [.precision]
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
 
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
 
-        System.out.printf("%.3f\n", price1);
-        System.out.printf("%.3f\n", price2);
-        System.out.printf("%.3f\n", price3);
+        System.out.printf("The amount after %d years is $%.2f", years, amount);
 
-        // [flags]
-
-        // + = output a plus
-        // , = comma grouping separator
-        // ( = negative numbers are enclosed in ()
-        // space = display a minus if negative, space if positive
-
-        System.out.printf("%f\n", price1);
-        System.out.printf("%f\n", price2);
-        System.out.printf("%f\n", price3);
-
-        // [width]
-
-        // 0 = zero padding
-        // number = right justified padding
-        // negative number = left justified padding
-
-        int id1 = 1;
-        int id2 = 23;
-        int id3 = 456;
-        int id4 = 7890;
-
-        System.out.printf("id: %04d\n", id1);
-        System.out.printf("id: %04d\n", id2);
-        System.out.printf("id: %04d\n", id3);
-        System.out.printf("id: %04d\n", id4);
+        scanner.close();
     }
 }
