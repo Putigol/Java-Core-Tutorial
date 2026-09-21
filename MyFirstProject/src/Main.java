dfc30edd401912c51590679fe7e7c272631fa10e
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-21 15:17:20 +0700

    ex1

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,24 +1,20 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
 
-        Random random = new Random();
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
 
-        int number;
-        boolean isHeads;
+        double result;
 
-        number = random.nextInt(1, 7);
+        result = Math.pow(3, 4);
+        result = Math.abs(-5);
+        result = Math.sqrt(16);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.14);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
 
-        isHeads = random.nextBoolean();
-
-        System.out.println(number);
-
-        if(isHeads){
-            System.out.println("HEADS");
-        }
-        else{
-            System.out.println("TAILS");
-        }
+        System.out.println(result);
     }
 }
