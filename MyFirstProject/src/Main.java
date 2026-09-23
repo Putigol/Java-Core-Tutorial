0cb3cae9742b65ea0480c987ed784d7fd73d6ffa
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-23 17:51:47 +0700

    Substrings

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,37 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String name = "Bro Code";
+        // .substring() = A method used to extract a portion of a string
+        //                         .substring(start, end)
 
-        int length = name.length();
-        char letter = name.charAt(0);
-        int index = name.indexOf(" ");
-        int lastIndex = name.lastIndexOf("o");
+        Scanner scanner = new Scanner(System.in);
 
-        name = name.toUpperCase();
-        name = name.toLowerCase();
-        name = name.trim();
-        name = name.replace("o", "a");
+        String email;
+        String username;
+        String domain;
 
-        if(name.isEmpty()){
-            System.out.println("Your name is empty");
+        System.out.print("Enter your email: ");
+
+        email = scanner.nextLine();
+
+        if(email.contains("@")){
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println(username);
+            System.out.println(domain);
         }
         else{
-            System.out.println("Hello " + name);
+            System.out.println("Emails must contain @");
         }
 
-        if(name.contains(" ")){
-            System.out.println("Your name contains a space");
-        }
-        else{
-            System.out.println("Your name DOESN'T contain any spaces");
-        }
-
-        if(name.equalsIgnoreCase("password")){
-            System.out.println("Your name can't be password");
-        }
-        else{
-            System.out.println("Hello " + name);
-        }
+        scanner.close();
     }
 }
