580798d5b340604937c30acdeb97fd955a89ae4b
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-23 17:28:27 +0700

    Useful String methods

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,31 +1,37 @@
 public class Main {
     public static void main(String[] args) {
 
-        boolean isStudent = true;
-        boolean isSenior = true;
-        double price = 9.99;
+        String name = "Bro Code";
 
-        if(isStudent){
-            if(isSenior){
-                System.out.println("You get a senior discount of 20%");
-                System.out.println("You get a student discount of 10%");
-                price *= 0.7;
-            }
-            else{
-                System.out.println("You get a student discount of 10%");
-                price *= 0.9;
-            }
+        int length = name.length();
+        char letter = name.charAt(0);
+        int index = name.indexOf(" ");
+        int lastIndex = name.lastIndexOf("o");
+
+        name = name.toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim();
+        name = name.replace("o", "a");
+
+        if(name.isEmpty()){
+            System.out.println("Your name is empty");
         }
         else{
-            if(isSenior){
-                System.out.println("You get a senior discount of 20%");
-                price *= 0.8;
-            }
-            else{
-                price *= 1;
-            }
+            System.out.println("Hello " + name);
         }
 
-        System.out.printf("The price of a ticket is: $%.2f", price);
+        if(name.contains(" ")){
+            System.out.println("Your name contains a space");
+        }
+        else{
+            System.out.println("Your name DOESN'T contain any spaces");
+        }
+
+        if(name.equalsIgnoreCase("password")){
+            System.out.println("Your name can't be password");
+        }
+        else{
+            System.out.println("Hello " + name);
+        }
     }
 }
