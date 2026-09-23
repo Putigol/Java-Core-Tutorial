aa6ee1f2e84117cdfd28974f10667f8a3c0b51e7
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-23 18:04:31 +0700

    Code a Weight Converter

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -3,28 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // .substring() = A method used to extract a portion of a string
-        //                         .substring(start, end)
+        // WEIGHT CONVERSION PROGRAM
 
         Scanner scanner = new Scanner(System.in);
 
-        String email;
-        String username;
-        String domain;
+        double weight;
+        double newWeight;
+        int choice;
 
-        System.out.print("Enter your email: ");
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert lbs to kgs");
+        System.out.println("2: Convert kgs to lbs");
 
-        email = scanner.nextLine();
+        System.out.print("Choose an option: ");
+        choice = scanner.nextInt();
 
-        if(email.contains("@")){
-            username = email.substring(0, email.indexOf("@"));
-            domain = email.substring(email.indexOf("@") + 1);
-
-            System.out.println(username);
-            System.out.println(domain);
+        if(choice == 1){
+            System.out.print("Enter the weight in lbs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 0.453592;
+            System.out.printf("The new weight in kgs is: %.2f", newWeight);
+        }
+        else if(choice == 2){
+            System.out.print("Enter the weight in kgs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.20462;
+            System.out.printf("The new weight in lbs is: %.2f", newWeight);
         }
         else{
-            System.out.println("Emails must contain @");
+            System.out.println("That was not a valid choice");
         }
 
         scanner.close();
