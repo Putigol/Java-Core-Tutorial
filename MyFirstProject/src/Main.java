61532679694ddfd79a842acf22aa102560e50555
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-24 14:43:41 +0700

    Logical operators (weather)

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -2,46 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int temp = 20;
+        boolean isSunny = true;
 
-        double num1;
-        double num2;
-        char operator;
-        double result = 0;
-        boolean validOperation = true;
-
-        System.out.print("Enter the first number: ");
-        num1 = scanner.nextDouble();
-
-        System.out.print("Enter an operator (+, -, *, /, ^): ");
-        operator = scanner.next().charAt(0);
-
-        System.out.print("Enter the second number: ");
-        num2 = scanner.nextDouble();
-
-        switch (operator) {
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> {
-                if (num2 == 0) {
-                    System.out.println("Cannot divide by zero");
-                    validOperation = false;
-                } else {
-                    result = num1 / num2;
-                }
-            }
-            case '^' -> result = Math.pow(num1, num2);
-            default -> {
-                System.out.println("Invalid operator");
-                validOperation = false;
-            }
+// Sử dụng && (AND)
+        if (temp <= 30 && temp >= 0 && isSunny) {
+            System.out.println("The weather is good and it is sunny outside");
         }
-
-        if (validOperation) {
-            System.out.println("The result is " + result);
+// Sử dụng ! (NOT)
+        else if (temp <= 30 && temp >= 0 && !isSunny) {
+            System.out.println("It is cloudy outside");
         }
-
-        scanner.close();
+// Sử dụng || (OR)
+        else if (temp > 30 || temp < 0) {
+            System.out.println("The weather is bad");
+        }
     }
 }
