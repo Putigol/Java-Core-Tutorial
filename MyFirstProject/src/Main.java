cb663fae2a7a6e8eb6e55ad4ebfbf12a55282f9f
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-09-26 17:45:28 +0700

    Nested loops

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -1,23 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        for (int i = 1; i <= 10; i++) {
-//            if (i == 5) {
-//                break; // Thoát vòng lặp khi i = 5
-//            }
-//            System.out.println("Gia tri i: " + i);
-//        }
-//        System.out.println("Da thoat vong lap!");
-        for (int i = 1; i <= 5; i++) {
-            if (i == 3) {
-                continue; // Bỏ qua bước in khi i = 3, chuyển sang i = 4
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the number of rows: ");
+        rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        columns = scanner.nextInt();
+        System.out.print("Enter the symbol to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for (int i = 0; i < rows; i++) {
+            System.out.println();
+            for (int j = 0; j < columns; j++) {
+                System.out.print(symbol);
             }
-            System.out.println("Gia tri i: " + i);
         }
 
+        scanner.close();
     }
 }
-
-
-
-
-
